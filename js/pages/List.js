4adde19113d1c9d31413282503aa6c38381711da
c0sm1c <theduckling2000@gmail.com>
2026-03-03 02:04:43 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -54,6 +54,10 @@ export default {
                             <div class="type-title-sm">Password</div>
                             <p>{{ level.password || 'Free to Copy' }}</p>
                         </li>
+                         <li>
+                            <div class="type-title-sm">HandCam</div>
+                            <p>{{ level.Handcam || 'Not Needed ' }}</p>
+                        </li>
                     </ul>
                     <h2>Records</h2>
                     <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
